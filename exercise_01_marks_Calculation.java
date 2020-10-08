afcf9fb7181d7ebef3662b1efc116522863a410b
Praveen S Chauhan <70374163+praveen13c@users.noreply.github.com>
2020-10-08 11:14:18 +0530

    Update exercise_01_marks_Calculation.java
    subjects now real and relevant and make all inputs float so that any marks will be in decimals , if any teacher wants to

--- exercise_01_marks_Calculation.java
+++ exercise_01_marks_Calculation.java
@@ -12,16 +12,16 @@ public class exercise_01_marks_Calculation {
         // making a percentage calculator on the basis of 5 subjects marks
         System.out.println("Calculate Marks of Student ");
         System.out.println("Enter Marks of  ");
-        System.out.println("Subject 01 (out of 100)");
-        int sub1 = scm.nextInt();
-        System.out.println("Subject 02 (out of 100)");
-        int sub2 = scm.nextInt();
-        System.out.println("Subject 03 (out of 100)");
-        int sub3 = scm.nextInt();
-        System.out.println("Subject 04 (out of 100)");
-        int sub4 = scm.nextInt();
-        System.out.println("Subject 05 (out of 100)");
-        int sub5 = scm.nextInt();
+        System.out.println("Subject 'Hindee' (out of 100)");
+        float sub1 = scm.nextInt();
+        System.out.println("Subject 'English (out of 100)");
+        float sub2 = scm.nextInt();
+        System.out.println("Subject 'Science (out of 100)");
+        float sub3 = scm.nextInt();
+        System.out.println("Subject 'Computer' (out of 100)");
+        float sub4 = scm.nextInt();
+        System.out.println("Subject 'Environment (out of 100)");
+        float sub5 = scm.nextInt();
 
         float marks = sub1+sub2+sub3+sub4+sub5; // first calculate all marks in a variable
 
